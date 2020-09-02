cb058fbf83f0c48431029db3b475335612f8abd9
papadaanjua <papadaanjua18@gmail.com>
2020-09-02 15:23:07 +0800

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,4 @@
+module.exports = ({ env }) => ({
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '64f8e511-7ff7-44e3-a278-99026b4aa335'
+};
